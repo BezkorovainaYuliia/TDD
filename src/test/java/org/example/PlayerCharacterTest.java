0eb5a84e52c4f0f8a12c8af7a75193fd99da28dc
Yuliia Bezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-06-27 16:30:50 +0200

    Zweite Minimale Implementierung, Test

--- src/test/java/org/example/PlayerCharacterTest.java
+++ src/test/java/org/example/PlayerCharacterTest.java
@@ -4,12 +4,19 @@ import org.junit.jupiter.api.Test;
 
 import static org.junit.jupiter.api.Assertions.*;
 
-class AufgabeTDDTest {
+class PlayerCharacterTest {
     @Test
     void isPositionXZero()
     {
         int expectedX = 0;
         assertEquals(expectedX, PlayerCharacter.getX());
+    }
+
+    @Test
+    void isPositionYZero()
+    {
+        int expectedY = 0;
+        assertEquals(expectedY, PlayerCharacter.getY());
     }
 
 }
