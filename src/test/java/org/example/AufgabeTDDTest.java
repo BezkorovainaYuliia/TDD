ddf36c4515ab9bc55930f4ad15155c6fbbfe6496
Yuliia Bezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-06-27 16:27:04 +0200

    Minimale Implementierung, Test

--- src/test/java/org/example/AufgabeTDDTest.java
+++ src/test/java/org/example/AufgabeTDDTest.java
@@ -1,7 +1,15 @@
 package org.example;
 
+import org.junit.jupiter.api.Test;
+
 import static org.junit.jupiter.api.Assertions.*;
 
 class AufgabeTDDTest {
+    @Test
+    void isPositionXZero()
+    {
+        int expectedX = 0;
+        assertEquals(expectedX, PlayerCharacter.getX());
+    }
 
 }
