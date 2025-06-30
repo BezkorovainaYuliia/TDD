547b9f4ec32ff6e5510b8e98bc7c13f99e4a0b42
Yuliia Bezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-06-30 11:40:18 +0200

    Fibonacci class und Testen bis zum 12 Fertig

--- src/test/java/org/example/FibonacciTest.java
+++ src/test/java/org/example/FibonacciTest.java
@@ -8,7 +8,7 @@ class FibonacciTest {
     @Test
     void Fibonachi0() {
         int expected = 0;
-        int actual = Fibonacci.Fibonacci(0);
+        int actual = Fibonacci.fibonacciFunc(0);
         assertEquals(expected, actual);
 
     }
@@ -16,7 +16,7 @@ class FibonacciTest {
     @Test
     void Fibonachi1() {
         int expected = 1;
-        int actual = Fibonacci.Fibonacci(1);
+        int actual = Fibonacci.fibonacciFunc(1);
         assertEquals(expected, actual);
 
     }
@@ -24,7 +24,7 @@ class FibonacciTest {
     @Test
     void Fibonachi2() {
         int expected = 1;
-        int actual = Fibonacci.Fibonacci(2);
+        int actual = Fibonacci.fibonacciFunc(2);
         assertEquals(expected, actual);
 
     }
@@ -32,7 +32,7 @@ class FibonacciTest {
     @Test
     void Fibonachi3() {
         int expected = 2;
-        int actual = Fibonacci.Fibonacci(3);
+        int actual = Fibonacci.fibonacciFunc(3);
         assertEquals(expected, actual);
 
     }
@@ -40,14 +40,14 @@ class FibonacciTest {
     @Test
     void Fibonachi4() {
         int expected = 3;
-        int actual = Fibonacci.Fibonacci(4);
+        int actual = Fibonacci.fibonacciFunc(4);
         assertEquals(expected, actual);
 
     }
     @Test
     void Fibonachi5() {
         int expected = 5;
-        int actual = Fibonacci.Fibonacci(5);
+        int actual = Fibonacci.fibonacciFunc(5);
         assertEquals(expected, actual);
 
     }
@@ -55,7 +55,7 @@ class FibonacciTest {
     @Test
     void Fibonachi6() {
         int expected = 8;
-        int actual = Fibonacci.Fibonacci(6);
+        int actual = Fibonacci.fibonacciFunc(6);
         assertEquals(expected, actual);
 
     }
@@ -63,7 +63,7 @@ class FibonacciTest {
     @Test
     void Fibonachi7() {
         int expected = 13;
-        int actual = Fibonacci.Fibonacci(7);
+        int actual = Fibonacci.fibonacciFunc(7);
         assertEquals(expected, actual);
 
     }
@@ -71,7 +71,7 @@ class FibonacciTest {
     @Test
     void Fibonachi8() {
         int expected = 21;
-        int actual = Fibonacci.Fibonacci(8);
+        int actual = Fibonacci.fibonacciFunc(8);
         assertEquals(expected, actual);
 
     }
@@ -79,7 +79,7 @@ class FibonacciTest {
     @Test
     void Fibonachi12() {
         int expected = 144;
-        int actual = Fibonacci.Fibonacci(12);
+        int actual = Fibonacci.fibonacciFunc(12);
         assertEquals(expected, actual);
 
     }
