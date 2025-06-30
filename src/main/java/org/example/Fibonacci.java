51ee540d3ea87b45198fdb36a4091f227fda5da1
Yuliia Bezkorovaina <yuliya.bezkorovayna@gmail.com>
2025-06-30 11:00:50 +0200

    Fibonacci class und Testen bis zum 12 Fertig

--- src/main/java/org/example/Fibonacci.java
+++ src/main/java/org/example/Fibonacci.java
@@ -2,8 +2,16 @@ package org.example;
 
 public class Fibonacci {
 public static int Fibonacci(int x) {
-    if (x < 2) { return x; }
-    if (x == 2) { return 1; }
+    if (x <= 1) { return x;}
+    int vorLetzteFib = 1;
+    if (x >= 2) {
+        return fibonacci(x);
+    }
     return 0;
+}
+
+static int fibonacci(int x) {
+    if (x <= 1) { return x;}
+    return fibonacci(x - 1) + fibonacci(x - 2);
 }
 }
--- src/test/java/org/example/FibonacciTest.java
+++ src/test/java/org/example/FibonacciTest.java
@@ -29,4 +29,58 @@ class FibonacciTest {
 
     }
 
+    @Test
+    void Fibonachi3() {
+        int expected = 2;
+        int actual = Fibonacci.Fibonacci(3);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void Fibonachi4() {
+        int expected = 3;
+        int actual = Fibonacci.Fibonacci(4);
+        assertEquals(expected, actual);
+
+    }
+    @Test
+    void Fibonachi5() {
+        int expected = 5;
+        int actual = Fibonacci.Fibonacci(5);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void Fibonachi6() {
+        int expected = 8;
+        int actual = Fibonacci.Fibonacci(6);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void Fibonachi7() {
+        int expected = 13;
+        int actual = Fibonacci.Fibonacci(7);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void Fibonachi8() {
+        int expected = 21;
+        int actual = Fibonacci.Fibonacci(8);
+        assertEquals(expected, actual);
+
+    }
+
+    @Test
+    void Fibonachi12() {
+        int expected = 144;
+        int actual = Fibonacci.Fibonacci(12);
+        assertEquals(expected, actual);
+
+    }
 }
